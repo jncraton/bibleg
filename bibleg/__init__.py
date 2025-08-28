7c1e2090bd4201ca12bd23e62b6fa9951e3102ad
Jon Craton <jncraton@gmail.com>
2025-08-28 08:55:29 -0400

    Extract html chunk

--- bibleg/__init__.py
+++ bibleg/__init__.py
@@ -1,7 +1,15 @@
 import requests
+import re
 
 def get_text(ref, version="ESV"):
     res = requests.get(f"https://www.biblegateway.com/passage/?search={ref}&version={version}")
 
     assert res.status_code == 200
-    return res.text
+
+    html = res.content.decode()
+
+    m = re.search(r'<div class="passage-text">(.*?)</div>', html, flags=re.I|re.DOTALL|re.M)
+
+    assert m != None
+
+    return m.group(0)
