42810a39b514c61777d8a96e0b5a4becb4fa682f
Jon Craton <jncraton@gmail.com>
2025-08-31 16:08:50 -0400

    Match whole reference only

--- bibleg/__init__.py
+++ bibleg/__init__.py
@@ -162,7 +162,7 @@ def normalize_verse_ref(ref):
     (47, 1, 2)
     """
 
-    book_chapter_verse = re.match(r"(.*?)[\t \.]*(\d+):(\d+)", ref.strip().title())
+    book_chapter_verse = re.match(r"(.*?)[\t \.]*(\d+):(\d+)$", ref.strip().title())
 
     if book_chapter_verse:
         book_idx = None
